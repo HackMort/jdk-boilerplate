ca9324a7890c87abf4877c353090f82cb09a0c48
Jonathan Juarez <jonathan.juarezwp@gmail.com>
2023-04-21 15:56:30 -0600

    Add Cookie Styles file.

---
+++ public/assets/js/validate.js
@@ -0,0 +1,32 @@
+/**
+  * The function below is used to validate the access code for the site
+  * @see public/assets/js/validate.js
+  * The function is called from public/assets/js/script.js
+  * When the user submits the form, the function checks the value of the input field against the access code (customPSW)
+  * If the access code is correct, the function sets a cookie with the access code and redirects the user to the home page
+  * If the access code is incorrect, the function displays an alert
+  * The function also checks if the user has a cookie with the access code
+  * If the user does not have a cookie with the access code, the function redirects the user to the validation page
+  */
+
+export function Validation () {
+  const accessCookie = document.cookie.split(';').filter((item) => item.trim().startsWith('accessCookie=')).pop()
+  if (!accessCookie && accessCookie !== 'accessCookie=customPSW' && window.location.pathname !== '/validate/') {
+    window.location.href = '/validate/'
+  }
+
+  // Validate Form
+  const form = document.querySelector('.form__validate')
+  if (form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      const accessCode = form.querySelector('input[name="access-code"]').value
+      if (accessCode === 'customPSW') {
+        document.cookie = `accessCookie=${accessCode};max-age=604800;path=/`
+        window.location.href = '/'
+      } else {
+        window.alert('Invalid Access Code')
+      }
+    })
+  }
+}
--- public/js/script.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('DOM fully loaded and parsed')
-})
