a81423373055c298bdc34fd44b0837ce40321c9f
Anderson Castellon <anderson.ivan.fuentes@gmail.com>
2023-06-08 09:22:05 -0600

    ✨ Add modal initial structure

---
+++ src/components/modal/jd-modal.ts
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body')
+  console.log(
+    '🚀 ~ file: jd-modal.ts:3 ~ document.addEventListener ~ body:',
+    body
+  )
+})
