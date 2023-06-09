fa34d1bc68b750cc6610c0bc5ebf9670c68cf555
Jhossymar Contreras <christian.contreras@jdoutstanding.com>
2023-06-09 16:31:53 -0600

    feat: :sparkles: Add files for Internal Navigation component
    Add astro, scss, js, and data files for Internal Navigation component

---
+++ src/components/Navigation/data/internal_navigation-data.js
@@ -0,0 +1,29 @@
+/*
+  elementsBeforeNav: Allows to know containers that will have position:fixed over navigation to calculate correct position to scroll and correct top position if navigation bar has position: fixed
+
+  navigationItems: Allows to set navigation items for the list on each page on site
+*/
+const navigationData = {
+  elementsBeforeNav: [
+    { mainSelector: '.test-section-a', fixedPositionClassname: 'is--fixed' },
+    { mainSelector: '.test-section-b', fixedPositionClassname: 'is--fixed' }
+  ],
+  navigationItems: {
+    design: [
+      { href: 'design-colors', label: 'Colors', active: true },
+      { href: 'design-fonts', label: 'Fonts' },
+      { href: 'design-buttons', label: 'Buttons' },
+      { href: 'design-tabs', label: 'Tabs' },
+      { href: 'design-accordions', label: 'Accordions' }
+    ],
+    'internal-navigation-test': [
+      { href: 'colors', label: 'Colors', active: true },
+      { href: 'fonts', label: 'Fonts' },
+      { href: 'buttons', label: 'Buttons' },
+      { href: 'tabs', label: 'Tabs' },
+      { href: 'accordions', label: 'Accordions' }
+    ]
+  }
+}
+
+export default navigationData
