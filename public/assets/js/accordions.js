bf2f9214e8c009deb0cbb38c28be7cc2cd8fb9af
elian-dev <geordymemdoza@gmail.com>
2023-06-09 11:25:40 -0600

    refactor: :recycle: Improve accordions component code
    Improve accordions code, modified the function code in order to be more reusable & added documentation to astro the component

--- public/assets/js/accordions.js
+++ public/assets/js/accordions.js
@@ -14,56 +14,60 @@
 export function Accordions () {
   const accordions = document.querySelectorAll('.accordion-group')
 
-  if (accordions) {
-    accordions.forEach((accordion) => {
-      const accordionIsExpandAll = accordion.hasAttribute('data-expanded') ?? false
-      const accordionItems = accordion.querySelectorAll('.accordion__item')
-      const accordionTrigger = accordion.querySelector('.accordion-group-toggle') ?? false
+  if (!accordions) {
+    return
+  }
 
-      if (accordionIsExpandAll) {
-        accordionTrigger && accordionTrigger.classList.add('accordion-group-toggle--opened')
+  accordions.forEach((accordion) => {
+    const accordionIsExpandAll = accordion.hasAttribute('data-expanded') ?? false
+    const accordionItems = accordion.querySelectorAll('.accordion__item')
+    const accordionTrigger = accordion.querySelector('.accordion-group-toggle') ?? false
+    const accordionTriggerOpenClass = 'accordion-group-toggle--opened'
+
+    if (accordionIsExpandAll) {
+      accordionTrigger && accordionTrigger.classList.add(accordionTriggerOpenClass)
+      accordionItems.forEach((accordionItem) => {
+        accordionItem.setAttribute('aria-expanded', true)
+      })
+    }
+
+    accordionItems.forEach((accordionItem) => {
+      accordionItem.addEventListener('click', (e) => {
+        const expanded = accordionItem.getAttribute('aria-expanded') === 'true' || false
+        accordionItem.setAttribute('aria-expanded', !expanded)
+        // close all other accordion items... maybe?
+        /* accordionItems.forEach((accordionItem) => {
+          // If the accordion item is not the clicked and if its not expanded
+          if (accordionItem !== e.currentTarget && expanded === false) {
+            accordionItem.setAttribute('aria-expanded', false)
+          }
+        }) */
+
+        if (accordionTrigger) {
+          if (expanded) {
+            accordionTrigger.classList.remove(accordionTriggerOpenClass)
+          } else {
+            accordionTrigger.classList.add(accordionTriggerOpenClass)
+          }
+        }
+      })
+    })
+
+    accordionTrigger && accordionTrigger.addEventListener('click', (e) => {
+      e.preventDefault()
+      const opened = accordionTrigger.classList.contains(accordionTriggerOpenClass)
+
+      if (opened) {
+        accordionTrigger.classList.remove(accordionTriggerOpenClass)
+        accordionItems.forEach((accordionItem) => {
+          accordionItem.setAttribute('aria-expanded', false)
+        })
+      } else {
+        accordionTrigger.classList.add(accordionTriggerOpenClass)
         accordionItems.forEach((accordionItem) => {
           accordionItem.setAttribute('aria-expanded', true)
         })
       }
-
-      accordionItems.forEach((accordionItem) => {
-        accordionItem.addEventListener('click', (e) => {
-          const expanded = accordionItem.getAttribute('aria-expanded') === 'true' || false
-          accordionItem.setAttribute('aria-expanded', !expanded)
-          // close all other accordion items... maybe?
-          /* accordionItems.forEach((accordionItem) => {
-            if (accordionItem !== e.currentTarget) {
-              accordionItem.setAttribute('aria-expanded', false)
-            }
-          }) */
-
-          if (accordionTrigger) {
-            if (expanded) {
-              accordionTrigger.classList.remove('accordion-group-toggle--opened')
-            } else {
-              accordionTrigger.classList.add('accordion-group-toggle--opened')
-            }
-          }
-        })
-      })
-
-      accordionTrigger && accordionTrigger.addEventListener('click', (e) => {
-        e.preventDefault()
-        const opened = accordionTrigger.classList.contains('accordion-group-toggle--opened')
-
-        if (opened) {
-          accordionTrigger.classList.remove('accordion-group-toggle--opened')
-          accordionItems.forEach((accordionItem) => {
-            accordionItem.setAttribute('aria-expanded', false)
-          })
-        } else {
-          accordionTrigger.classList.add('accordion-group-toggle--opened')
-          accordionItems.forEach((accordionItem) => {
-            accordionItem.setAttribute('aria-expanded', true)
-          })
-        }
-      })
     })
-  }
+  })
 }
