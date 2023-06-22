22ae1e4b504035def08c2acf002c0c19eb819513
Anderson Castellon <anderson.ivan.fuentes@gmail.com>
2023-06-21 21:40:41 -0600

    ✨ Add modal event example

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -8,19 +8,23 @@ document.addEventListener('DOMContentLoaded', function (e) {
    * Validation
    * @see public/assets/js/validation.js
    * Call the function from validation.js
-  */
+   */
   cookieValidation()
   cookieFormValidation()
   /**
    * Tabs
    * @see public/assets/js/tabs.js
    * Call the function from tabs.js
-  */
+   */
   Tabs()
   /**
    * Accordions
    * @see public/assets/js/accordions.js
    * Call the function from accordions.js
-  */
+   */
   Accordions()
+
+  document.addEventListener('modal:done', ({ detail }) => {
+    console.log(detail.id)
+  })
 })
